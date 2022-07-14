736a7897d452a47a3ee829caf53f4761673a23bf
Nicolas Cruz <cruznicolas@protonmail.ch>
2022-07-13 23:20:28 -0500

    Fix not grid not centering

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 function createGrid (size, container) {
+	container.innerHTML = "";
 	for (let i = 0; i < size; i++) {
 		let divrow = document.createElement("div");
 		divrow.className = "row";
 		for (let j= 0; j < size; j++) {
 			divcell = document.createElement("div");
 			divcell.className = "cell";
+			divcell.addEventListener('mouseover', tint)
 			//divcell.innerText = "celda";
 			divrow.appendChild(divcell);
 		}
@@ -16,19 +18,24 @@ function getSize () {
 	size = prompt();
 }
 
+function clearGrid () {
+	
+}
+
 function tint() {
 this.classList.add("hovered");
 }
 
 gridcontainer = document.querySelector('.container');
+createGrid(10,gridcontainer);
 
-button = document.querySelector('button');
-button.addEventListener('click', function () {let size = prompt("Digite tamaño"); createGrid(size =5,gridcontainer);let cells = document.querySelectorAll('.cell');} );
-
-
-
+// button = document.querySelector('button');
+// button.addEventListener('click', function () {let size = prompt("Digite tamaño"); createGrid(size =5,gridcontainer);let cells = document.querySelectorAll('.cell');} );
 
 
-cells.forEach(cell => cell.addEventListener('mouseover', tint()))
+
+
+
+//cells.forEach(cell => cell.addEventListener('mouseover', tint()))
 
 
